0901edbde0a0bb8547fd01bd2be542048d29723c
Stanislav2022 <tsv5@ukr.net>
2023-04-21 13:21:53 +0300

    1

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,46 @@
+import css from "../Phonebook.module.css"
+import { AiFillDelete, AiOutlineContacts } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteContact } from "redux/contacts/contacts-operation";
+import { getContacts } from "redux/contacts/contacts-selector";
+import { getFilter } from "redux/filter/filter-selector";
+;
+
+export const ContactList = () => {
+  const contacts = useSelector(getContacts);
+//   const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+
+//   const getFiltereContacts = () => {
+//       if (!filter) {
+//           return contacts;
+//       }
+//       const normalizedFilter = filter.toLocaleLowerCase();       
+//       const filteredContacts = contacts.filter(({ name, phone }) => {
+//           const normalizedName = name.toLocaleLowerCase();
+//           const normalizedNumber = phone.toLocaleLowerCase();
+//           const result = normalizedName.includes(normalizedFilter) || normalizedNumber.includes(normalizedFilter);
+//           return result;
+//       })   
+//       return filteredContacts;
+//   }
+//   const handleDelete  = (id) => {
+//       const action = deleteContact(id);
+//       dispatch(action);
+//   }
+
+//   const filterContacts = getFiltereContacts();
+
+  const element = contacts.map(({avatar, user, id, tweets, followers}) => {
+      return <li className={css.contacts__list} key={id}>
+          {user}: {tweets} {followers}
+            </li>
+    })
+  
+  return (
+    <>
+      <h2 className={css.header}>Contacts list <AiOutlineContacts/></h2>
+      <ul>{element}</ul>
+    </>
+  )
+}
