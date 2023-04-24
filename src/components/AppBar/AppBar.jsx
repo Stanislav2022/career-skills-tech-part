c7f9759467bae551728b0d6ff1ec14c35d78be8a
Stanislav2022 <tsv5@ukr.net>
2023-04-24 22:01:14 +0300

    1

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,14 +1,11 @@
-import css from './AppBar.module.css';
 import { NavLink } from 'react-router-dom';
 
 export const AppBar = () => {
 
   return (
-      <div>
-        <header className={css.header}>
+        <header>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/cards">Cards</NavLink>
         </header>
-      </div>
   );
 };
